53c967c84e2deb8eb0647516a419034b8705fb01
Stephan Risi <stephan.risi@gmail.com>
2019-07-12 07:29:39 -0400

    updated search

--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import style from "./html/style.css";
 
 // require('!style-loader!css-loader!marx-css/css/marx.css')
 
+const d3 = require('d3')
+window.d3 = d3;
 import simulation from './js/simulation.js'
 import { s } from './js/state'
+//import update_researcher_autocomplete from './js/search.js'
 
-const graph = require('./data/network.json');
+import {init_researcher_autocomplete} from "./js/search.js";
 
-console.log(graph)
 
-s.setCanvas()
-s.setGraph(graph)
-s.setPairs(graph.nodes)
-s.setScreen()
-simulation()
+window.onload = function () {
+    const graph = require('./data/network.json');
+
+    s.setCanvas();
+    s.setGraph(graph);
+    s.setPairs(graph.nodes);
+    s.setScreen();
+//    simulation();
+    init_researcher_autocomplete();
+
+    d3.select('.autocomplete').on('click', function(){
+        console.log('here', this);
+    });
+};
+
+
 
