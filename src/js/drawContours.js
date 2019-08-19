ce22ca00a445413a633c30c024b7f4c2d6b29b8c
Dario Rodighiero <fumoseaffabulazioni@gmail.com>
2019-08-19 10:09:14 +0200

    Update drawContours.js

--- src/js/drawContours.js
+++ src/js/drawContours.js
@@ -1,7 +1,7 @@
 import * as d3 from 'd3'
 import { s } from './state'
 
-const z0 = { x: 0, y: 0, k: 0 }
+const z = { x: 0, y: 0, k: 0 }
 
 const _computeDensityData = () => {
 
@@ -10,24 +10,27 @@ const _computeDensityData = () => {
         em = Math.max(ex[1] - ex[0], ey[1] - ey[0])
 
     // const w = 4 * s.screen.width; // definition of the grid for the contours
-    const w = ex[1] - ex[0] // This is important for the computation weight, it should be zoomable
-    z0.k = w / em
-    z0.x = -ex[0] * z0.k;
-    z0.y = -ey[0] * z0.k;
+    const width = ex[1] - ex[0] // This is important for the computation weight, it should be zoomable
+    const height = ey[1] - ey[0] // This is important for the computation weight, it should be zoomable
+    console.log(ex, ey, em)
+    // console.log(width, height)
+    z.k = width / em
+    z.x = -ex[0] * z.k
+    z.y = -ey[0] * z.k
 
     s.densityData = d3.contourDensity()
-        .x(d => z0.x + d.x * z0.k)
-        .y(d => z0.y + d.y * z0.k)
-        // .weight(d => z0.k * d.docs) // Old version
+        .x(d => z.x + d.x * z.k)
+        .y(d => z.y + d.y * z.k)
         .weight(d => d.docs)
-        .size([w, w])
-        // .bandwidth(40 * z0.k)
-        .thresholds(10) // Indivative number of levels
+        .size([width, height])
+        .cellSize(1) // Crispness (1 = best resolution)
+        .bandwidth(20) // Expansion of reliefs (40 = high simplification)
+        .thresholds(10) // Indicative number of levels
         (s.graph.nodes)
 
     s.densityData.forEach(d => d.coordinates = d.coordinates
         .map(d => d.map(d => d.map(
-            d => [(d[0] - z0.x) / z0.k, (d[1] - z0.y) / z0.k]
+            d => [(d[0] - z.x) / z.k, (d[1] - z.y) / z.k]
         )))
     )
 }
@@ -38,21 +41,15 @@ export default () => {
 
     if (s.end && !s.densityData.length) _computeDensityData()
     // _computeDensityData()
-
     const path = d3.geoPath().context(s.context)
 
-    // console.log(s.densityData)
-
-
     s.densityData
-        // .slice(0, 10)
         .forEach((level, i) => {
             s.context.beginPath()
             s.context.strokeStyle = color
-            s.context.lineWidth = (.1 + (.05 * i)) / s.zoomIdentity.k
+            s.context.lineWidth = (.8 + (.1 * i))
             path(level)
             s.context.stroke()
         })
-
 
 }
