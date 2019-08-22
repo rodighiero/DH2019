3af4c360fcdc4c083ecb726351edeff59b288050
Dario Rodighiero <fumoseaffabulazioni@gmail.com>
2019-08-22 23:42:51 +0200

    Simplification contour

--- src/js/drawContours.js
+++ src/js/drawContours.js
@@ -1,35 +1,30 @@
 import * as d3 from 'd3'
 import { s } from './state'
 
-const z = { x: 0, y: 0, k: 0 }
-
 const _computeDensityData = () => {
 
-    const ex = d3.extent(s.nodes, d => d.x),
-        ey = d3.extent(s.nodes, d => d.y),
-        em = Math.max(ex[1] - ex[0], ey[1] - ey[0])
+    const extX = d3.extent(s.nodes, d => d.x)
+    const extY = d3.extent(s.nodes, d => d.y)
 
-    // const w = 4 * s.screen.width; // definition of the grid for the contours
-    const width = ex[1] - ex[0]
-    const height = ey[1] - ey[0]
-    
-    z.k = width / em
-    z.x = -ex[0] * z.k
-    z.y = -ey[0] * z.k
+    const width = extX[1] - extX[0]
+    const height = extY[1] - extY[0]
+
+    const x = extX[0]
+    const y = extY[0]
 
     s.densityData = d3.contourDensity()
-        .x(d => z.x + d.x * z.k)
-        .y(d => z.y + d.y * z.k)
+        .x(d => d.x - x)
+        .y(d => d.y - y)
         .weight(d => d.docs)
-        .size([width/2, height/10])
-        .cellSize(2) // Crispness (1 = best resolution)
+        .size([width, height])
+        .cellSize(10) // Crispness (1 = best resolution)
         // .bandwidth(10) // Expansion of reliefs (40 = high simplification)
-        // .thresholds(6) // Indicative number of levels
+        // .thresholds(10) // Indicative number of levels
         (s.nodes)
 
     s.densityData.forEach(d => d.coordinates = d.coordinates
         .map(d => d.map(d => d.map(
-            d => [(d[0] - z.x) / z.k, (d[1] - z.y) / z.k]
+            d => [(d[0] + x), (d[1] + y)]
         )))
     )
 }
@@ -46,7 +41,8 @@ export default () => {
             s.context.beginPath()
             path(level)
             s.context.strokeStyle = s.colors.contours
-            s.context.lineWidth = (.8 + (.1 * i))
+            s.context.lineWidth = 1
+            // s.context.lineWidth = (.8 + (.1 * i))
             s.context.stroke()
         })
 
